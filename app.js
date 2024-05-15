9b8f5145a4c27594981c66c12dd05550062670f1
Omer Koyuncu <omerkoyuncutr@gnail.com>
2024-05-15 19:33:24 +0300

    models, single photo page, db connect added

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+//connect DB
+mongoose
+  .connect('mongodb://127.0.0.1:27017/pcat-test-db')
+  .then(() => console.log('DB Connected!'));
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (rew, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
 });
-app.get('/about', (rew, res) => {
+
+app.get('/photos/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+});
+
+app.get('/about', (req, res) => {
   res.render('about');
 });
-app.get('/add', (rew, res) => {
+
+app.get('/add', (req, res) => {
   res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  console.log(req.body);
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
